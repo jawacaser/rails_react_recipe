4311b631b4030e50a1ad856e13a7d76dfcba30b4
Andrew Wacaser <jawacaser@gmail.com>
2022-06-22 14:11:09 -0500

    Initial routes for CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      post 'recipes/create'
+      get '/show/:id', to: 'recipes#show'
+      delete '/destroy/:id', to: 'recipes#destroy'
+    end
+  end
   root 'homepage#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/*path' => 'homepage#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
