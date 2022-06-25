44847a7bf484e3a764ffa4b39fc865f50b646b9c
jawacaser <97850241+jawacaser@users.noreply.github.com>
2022-06-25 18:09:33 -0500

    update default img logo

--- db/migrate/20220622185551_create_recipes.rb
+++ db/migrate/20220622185551_create_recipes.rb
@@ -4,7 +4,7 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
       t.string :name, null: false
       t.text :ingredients, null: false
       t.text :instruction, null: false
-      t.string :image, default: 'https://raw.githubusercontent.com/do-community/react_rails_recipe/master/app/assets/images/Sammy_Meal.jpg'
+      t.string :image, default: 'https://lh3.googleusercontent.com/pw/AM-JKLWzpHEwkBfYDRJwjiLKXVC16AaGjeaCFxN7a1KsiWQqouGKcWR81zvVLEheFYaA35JO3Z9zc-FZlfVmSgYOS38KTz45HwiFhxIDCFiQVeqVrig1lebaATH7CUmgXpMl6ytR1apV0xSHqXd6as5LLcI=w828-h315-no?authuser=0'
 
       t.timestamps
     end
