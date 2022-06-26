7636152bfc265d0dbbfdedbd4737c162b2b64bae
jawacaser <97850241+jawacaser@users.noreply.github.com>
2022-06-26 13:49:31 -0500

    Bootstrap should be loaded through webpack now

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// if using importmap INSTEAD of webpacker, you would import bootstrap here:
+// import "bootstrap"
+
+// Added
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+require("stylesheets/application.scss")
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
-
+// ADDED THE FOLOWING
+const webpack = require("webpack")
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
+    Popper: ['popper.js', 'default']
+  }))
+// END
 module.exports = environment
